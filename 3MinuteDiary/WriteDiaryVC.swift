e6e762da6fe9833fc17c213a7ee87e60a78efd3d
Bono b Bono <adiosbono@naver.com>
2020-01-18 17:22:34 +0900

    일기쓰기 화면 날짜 포맷을 설정함...년- 월- 일 순으로

--- 3MinuteDiary/WriteDiaryVC.swift
+++ 3MinuteDiary/WriteDiaryVC.swift
@@ -54,7 +54,17 @@ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexP
     case 0:
         let cell = tableView.dequeueReusableCell(withIdentifier: "toolBarCell") as! ToolBarCell
         
-        cell.showDate.text = "\(self.sendedDate!)"
+        //날짜를 받은대로 출력하면 시분초까지 나오니까 원하는 포맷으로 설정하기
+        
+       
+        let dateFormatter = DateFormatter()
+        //dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+            //왠진모르겠지만 toGlobalTime안해주면...그냥 아무날짜 선택하지 않고 기본세팅 오늘날짜로 했을때 다음날짜가 일기쓰기 화면에 나옴...
+        let date = dateFormatter.string(from: (self.sendedDate?.toGlobalTime())!)
+        
+        print("dateFromString : \(date)")
+        cell.showDate.text = date
         
                    
                    //여기안에 디비에서 받아온 정보를 가지고 각 셀 안의 데이터를 입력해준다.
@@ -89,7 +99,7 @@ override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: Inde
  */
 //테이블 뷰의 섹션의 수 결정하는 메소드 따로 오버라이드 하지 않으면 기본값은 1임
 override func numberOfSections(in tableView: UITableView) -> Int {
-    return 2
+    return 6
     }
 //각 섹션 헤더에 들어갈 뷰를 정의하는 메소드. 섹션별 타이틀을 뷰 형태로 구성하는 메소드 1080
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -97,15 +107,15 @@ override func numberOfSections(in tableView: UITableView) -> Int {
         case 0:
             return "메뉴"
         case 1:
-            return "별칭"
+            return "나의 목표"
         case 2:
-            return "메모"
+            return "하고싶은일"
         case 3:
-            return "카드사용조건"
+            return "오늘 있었던일"
         case 4:
-            return "상점명"
+            return "감사할일"
         case 5:
-            return "혜택"
+            return "성공법칙"
         case 6:
             return "제약"
         default:
